52aca57514bb4859c6fc762099067a7ccbb315c2
christian dees <christianmdees@mgail.com>
2024-09-15 22:23:51 -0600

    finished redirection, needs piping + cleanup

--- shell.py
+++ shell.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 
+
 def findPath(command):
     paths = os.environ.get("PATH").split(":")
     for path in paths:
@@ -14,139 +15,147 @@ def findPath(command):
             continue
     return None
 
-def processType(args):
-    cmds = args.split(";")
-    backgroundProcesses = []
-    regularProcesses = []
-   
-    for cmd in cmds:
-        # Extract commands that are meant to run in the background
-        patternBP = r'[^&]*&'
-        matchesBP = re.findall(patternBP, cmd)
-       
-        # Extract remaining commands which are not meant to be in the background
-        cmd_cleaned = re.sub(patternBP, '', cmd).strip()  # Remove background commands from the original string
-        if cmd_cleaned:
-            regularProcesses = [cmd_cleaned]  # Assign the remaining commands as regular
-        for match in matchesBP:
-            backgroundProcesses.append(match[:-1].strip())
-           
-    return backgroundProcesses, regularProcesses
-
-       
-
 def runProcess(command):
-    cpid = os.fork()
-    if cpid == 0:
-        # Child process
-       
-        try:
-            # NEED TO CHANGE TO EXECV
-            os.execv(command[0], command)
-        except FileNotFoundError:
-            print(f"Command not found: {command[0]}")
-            sys.exit(1)
-    # Parent process
-    return cpid
+    try:
+        cpid = os.fork()
+        if cpid == 0:
+            # Child process
+            try:
+                os.execv(command[0], command)
+            except FileNotFoundError:
+                print(f"Command not found: {command[0]}")
+                sys.exit(1)
+        # Parent process
+        return cpid
+    except Exception as e:
+        print(f"Error executing process: {e}")
 
-if __name__ == "__main__":
-    #ps1 = os.environ.get('PS1', '$ ')
-    ps1=sys.path[0] +' $ '
-    inp = None
-    background_pids = []
-    def cdHome():
-         user_name = os.environ.get('USER') or os.environ.get('LOGNAME') or os.environ.get('USERNAME')
-         userPath = f"/home/{user_name}"
-         ps1 = userPath + ' $ '
-         os.chdir(userPath)
-         return ps1
-               
-    while inp != "quit":
-        inp = input(ps1).strip()
-        args = inp.split()
-        if not args or args[0] == "#":
-            continue
-        elif args[0] == "quit":
-            break
-        elif args[0] == "cd":
-            if len(args) > 1:
-                try:
-                    os.chdir(args[1])
-                    if (args[1][0] == '/'):
-                        ps1 = args[1] + ' $ '
-                    elif (args[1] == '.'):
-                        ps1 = goHome()
-                    elif ('..' in args[1]):
-                        totalCount = args[1].count('..')
-                        paths=ps1.split('/')
-                        start = len(paths)-1
-                        end = start-totalCount
-                       
-                        for i in range(start, end, -1):
-                            if len(paths)==2:
-                                paths[1] = ''
-                                break
-                            del paths[i]
-                        ps1 = '/'.join(paths) + ' $ '
-                         
+
+
+
+def handleRedirect(cmd, filename, mode, flags):
+    try:
+        pid = os.fork()
+        if pid == 0:
+            try:
+                fd = os.open(filename, flags)
+                if mode == 'in':
+                    os.dup2(fd, 0)  # Redirect stdin
+                elif mode == 'out':
+                    os.dup2(fd, 1)  # Redirect stdout
+                os.close(fd)  
+                os.execv(cmd[0], cmd)
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                return 1
+        elif pid > 0:
+            pid, status = os.waitpid(pid, 0)
+            if os.WIFEXITED(status):
+                return os.WEXITSTATUS(status)
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return 1
+
+
+
+
+
+
+
+def main():
+    try:
+        ps1 = os.getcwd() + ' $ '
+        inp = None
+        
+        background_pids = []
+                   
+        while True:
+            inp = input(ps1).strip()
+            if not inp:
+                continue
+            
+            ignorePat = r'#\s*([^;\n]+?)(?:;|$)'
+            inp = re.sub(ignorePat, ';', inp).strip(';')
+            splitPat = r';(?=(?:[^\'"]*\'[^\'"]*\')*[^\'"]*$)'
+            args = re.split(splitPat, inp)
+            
+            for arg in args:
+                arg = arg.replace("'",'')
+                cmdList = arg.split()
+                if not cmdList:
+                    continue
+                
+                cmd = cmdList[0].lower()
+                cidx = None
+                filename = None
+                flags = None
+                mode = None
+                
+                if cmd == 'quit':
+                    if background_pids:
+                        print("There are background processes running.")
+                        inp = input("Are you sure you want to exit when there are processes running in the background? (y/n) ").strip()
+                        if inp == "n":
+                            continue
+                        elif inp == "y":
+
+                            for pid in background_pids:
+                                os.waitpid(pid, 0)
+                    print("Exiting..")
+                    return
+                
+                elif cmd == 'cd':
+                    if len(cmdList) > 1:
+                        try:
+                            os.chdir(cmdList[1])
+                        except (NotADirectoryError, FileNotFoundError, PermissionError) as e:
+                            print(f"fash: cd: {cmdList[1]}: {str(e)}")
+                            continue
                     else:
-                        cwd = ps1.split(" ")[0]
-                        if args[1].endswith('/'):
-                            args[1] = args[1][:-1]
-                        if (cwd.endswith('/')):
-                            cwd+=args[1]
-                        else:
-                            cwd+='/'+args[1]
-                        ps1 = cwd + ' $ '
-                       
-                   
-                   
-                except NotADirectoryError:
-                    print(f"fash: cd: {args[1]}: Not a directory")
-                except FileNotFoundError:
-                    print(f"fash: cd: {args[1]}: No such file or directory")
-                except PermissionError:
-                    print(f"Permission denied: {args[1]}")
-            else:
-                ps1 = cdHome()
-        else:
-            path = findPath(args[0])
-            commandList = [path, args[1:]]
-            bgp, rp = processType(inp)
-            for process in bgp:
-                cmds = process.split()
-                path = findPath(cmds[0])
-                if len(cmds)>1:
-                    commandList = [path, ' '.join(cmds[1:])]
-                else:
-                    commandList = [path]
-                #print(f"COMANDLIST: {commandList}")
+                        os.chdir(os.environ.get('HOME'))
+                    ps1 = os.getcwd() + ' $ '
+                    
+                elif '<' in arg:
+                    cidx = arg.index('<')
+                    flags = os.O_RDONLY
+                    mode = 'in'
+                elif '>' in arg:
+                    cidx = arg.index('>')
+                    flags = os.O_WRONLY | os.O_CREAT
+                    mode = 'out'
+                    
+                if cidx:
+                    filename = arg[cidx+2:]
+                    arg = arg[:cidx]
+                    
+                cmdList = arg.split()
+                if not cmdList:
+                    continue
+                
+                cmd = cmdList[0].lower()
+                path = findPath(cmd)
                 if path:
-                    pid = runProcess(commandList)
-                    background_pids.append(pid)
-                    print(f"[{len(background_pids)}] {pid}")
+                    commandList = [path] + cmdList[1:]
+                    if filename:
+                        handleRedirect(commandList, filename, mode, flags)
+                    else:
+                        pType = "bg" if arg.endswith('&') else "fg"
+                        pid = runProcess(commandList)
+                        if pType == "bg":
+                            background_pids.append(pid)
+                        elif pType == "fg":
+                            pid, status = os.waitpid(pid, 0)
+                            if not os.WIFEXITED(status):
+                                print(f"Program terminated: exit code {status}.")
+                                sys.exit(1)
                 else:
-                    print("Command not found")
-               
-            for process in rp:
-                cmds = process.split()
-                path = findPath(cmds[0])
-               
-                if len(cmds)>1:
-                    commandList = [path, ' '.join(cmds[1:])]
-                else:
-                    commandList = [path]
-                #print(f"COMANDLIST: {commandList}")
-                if path:
-                    runProcess(commandList)            
-                    os.wait()
-                else:
-                    print("Command not found")
+                    print(f"fash: {cmd}: command not found")
+                
+    except KeyboardInterrupt:
+        print("\nExited on KeyboardInterrupt")
+        sys.exit(0)
 
-    while background_pids:
-        try:
-            pid, status = os.wait()  # Wait for any child process to complete
-            if pid in background_pids:
-                background_pids.remove(pid)
-        except ChildProcessError:
-            break
+  
+        
+if __name__ == "__main__":
+    main()
